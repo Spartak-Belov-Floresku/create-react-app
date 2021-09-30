e2943f41c43d27855fe3db5010bc93821ff38838
Spartak BF <job.spartak@gmail.com>
2021-09-29 21:33:17 -0400

    ES2015 Module Exercise

---
+++ src/helper.js
@@ -0,0 +1,19 @@
+class Hepler{
+
+    static choce(items) {
+        let fruit = items[Math.floor(Math.random()*Object.keys(items).length)];
+        return fruit;
+    }
+
+    static remove(items, item){
+        const check = Object.keys(items).length;
+        items.forEach((elm,i) => {
+            if(elm === item)
+                delete items[i]
+        });
+        return check !== Object.keys(items).length? item: undefined;
+    }
+    
+}
+
+export default Hepler
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { arr_foods } from './foods'
+import HeplerClass from './helper';
+
+
+const fruit1 = HeplerClass.choce(arr_foods);
+console.log(`I'd like one ${fruit1}, please`);
+const fruit2 = HeplerClass.remove(arr_foods, fruit1);
+console.log(`Here you go: ${fruit2}`);
+console.log(`Delicious! May I have another?`);
+console.log(`I'm sorry, we're all out. We have ${Object.keys(arr_foods).length} left.`)
+
