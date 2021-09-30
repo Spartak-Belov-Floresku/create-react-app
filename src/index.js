0305d1bea99b9d96579accf1f9505bf481ff11ec
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2021-09-29 23:33:14 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { arr_foods } from './foods'
-import HeplerClass from './helper';
+import HelperClass from './helper';
 
 
-const fruit1 = HeplerClass.choice(arr_foods);
+const fruit1 = HelperClass.choice(arr_foods);
 console.log(`I'd like one ${fruit1}, please`);
-const fruit2 = HeplerClass.remove(arr_foods, fruit1);
+const fruit2 = HelperClass.remove(arr_foods, fruit1);
 console.log(`Here you go: ${fruit2}`);
 console.log(`Delicious! May I have another?`);
 console.log(`I'm sorry, we're all out. We have ${Object.keys(arr_foods).length} left.`)
